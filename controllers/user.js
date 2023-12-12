ec6854b6a9ffafbc1075bac27d2940186e96363a
zRosies <gustavo.enrollment@gmail.com>
2023-12-11 21:50:59 -0300

    changes

--- controllers/user.js
+++ controllers/user.js
@@ -1,4 +1,5 @@
 const mongodb = require("../connection/db");
+const { ObjectId } = require("mongodb");
 
 const createPurchase = async (req, res) => {
   const purchaseInfo = {
@@ -9,12 +10,13 @@ const createPurchase = async (req, res) => {
       address: req.body.client.address,
     },
     paymentInfo: {
-      carsNum: req.body.paymentInfo.carsNum,
+      quantity: req.body.paymentInfo.quantity,
       delivery: req.body.paymentInfo.delivery,
       date: req.body.paymentInfo.date,
       fee: req.body.paymentInfo.fee,
       price: req.body.paymentInfo.price,
       carsName: req.body.paymentInfo.carsName,
+      method: req.body.paymentInfo.method,
     },
   };
 
@@ -26,9 +28,13 @@ const createPurchase = async (req, res) => {
       .insertOne(purchaseInfo);
 
     if (result && result.insertedId) {
-      res.status(201).json({ message: `${result.insertedId} added to the database` });
+      res
+        .status(201)
+        .json({ message: `${result.insertedId} added to the database` });
     } else {
-      res.status(400).json({ message: "No data found or error during insertion" });
+      res
+        .status(400)
+        .json({ message: "No data found or error during insertion" });
     }
   } catch (error) {
     console.log("Error querying the database:", error);
@@ -36,4 +42,42 @@ const createPurchase = async (req, res) => {
   }
 };
 
-module.exports = { createPurchase };
+const getUsers = async (req, res) => {
+  const result = await mongodb
+    .getDb()
+    .db("Veloster")
+    .collection("carRental")
+    .find()
+    .toArray();
+
+  if (result.length !== 0) {
+    res.setHeader("Content-Type", "application/json");
+    res.status(200).json(result);
+  } else {
+    res.status(400).json("Not found");
+  }
+};
+
+const getPurchaseById = async (req, res) => {
+  const userId = new ObjectId(req.params.id);
+  // console.log(userId)
+  const result = await mongodb
+    .getDb()
+    .db("Veloster")
+    .collection("carRental")
+    .findOne({ _id: userId });
+
+  try {
+    if (result.length === 0) {
+      res.status(404).json({ message: "No data found" });
+    } else {
+      res.setHeader("Content-Type", "application/json");
+      res.status(200).json(result);
+    }
+  } catch (error) {
+    console.error("Error querying the database:", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+module.exports = { createPurchase, getUsers, getPurchaseById };
--- routes/user.js
+++ routes/user.js
@@ -2,5 +2,7 @@ const router = require("express").Router();
 const user = require("../controllers/user.js");
 
 router.post("/", user.createPurchase);
+router.get("/", user.getUsers);
+router.get("/:id", user.getPurchaseById);
 
 module.exports = router;
