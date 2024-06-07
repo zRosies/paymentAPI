180a7a238e69e6735da17f619f1e0917037f2202
zRosies <gustavo.enrollment@gmail.com>
2024-06-07 18:22:42 -0300

    Adding schema for the swagger // 6 H

--- src/routes/user.js
+++ src/routes/user.js
@@ -4,8 +4,8 @@ const validation = require("../utils/validation.js");
 const authentication = require("../utils/authentication.js");
 
 router.post("/purchase", authentication, validation, user.createPurchase);
-router.get("/users", authentication, user.getUsers);
-router.get("/users/:id", authentication, user.getPurchaseById);
+router.get("/purchase", authentication, user.getUsers);
+router.get("/purchase/:id", authentication, user.getPurchaseById);
 router.delete("/delete/:id", authentication, user.deletePurchaseById);
 
 module.exports = router;
